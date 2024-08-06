1d154870296bd8e5b8ba1d2ed1e7f6c1d01fbcf8
Tobias S <tobias.seifert97@gmail.com>
2024-08-06 22:07:03 +0200

    2024-05-08 12:32  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/JustCombat/GameMode.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/JustCombat/Item.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/JustCombat/Player.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/JustCombat/Scenario.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/GameState.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/Net.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/Utils.lua  M JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/_Init.lua ?? JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/Event.lua

--- JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/GameState.lua
+++ JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/GameState.lua
@@ -3,110 +3,53 @@
 ---@type Utils
 local Utils = Require("Shared/Utils")
 
----@type Libs
-local Libs = Require("Shared/Libs")
+---@type Event
+local Event = Require("Shared/Event")
 
 ---@class GameState
 local M = {}
 
-local actions = {}
-
----@class StateAction : LibsObject
----@field Id string
----@field Mode number
----@field Once boolean
----@field Func fun()
----@field Exec fun(self:StateAction, e:table)
----@field Unregister fun(self:StateAction)
----@field New fun(mode:number, callback:fun(), once:boolean):StateAction
-local StateAction = Libs.Object({
-    Id = nil,
-    Mode = nil,
-    Once = false,
-    Func = function() end,
-    Exec = function(self, e)
-        xpcall(function()
-            self.Func(e)
-        end, function(err)
-            Utils.Log.Error(err)
-        end)
-
-        if self.Once then
-            self:Unregister()
-        end
-    end,
-    Unregister = function(self)
-        for i, a in pairs(actions) do
-            if a.Id == self.Id then
-                table.remove(actions, i)
-            end
-        end
-    end,
-})
-
-function StateAction.New(mode, callback, once)
-    local o = StateAction.Init({
-        Mode = mode,
-        Func = callback,
-        Once = once,
-    })
-
-    o.Id = tostring(o)
-
-    table.insert(actions, o)
-
-    return o
-end
-
-local modeSave = 1
-local modeLoad = 2
-local modeUnload = 3
+local modeSave = "GameStateSave"
+local modeLoad = "GameStateLoad"
+local modeUnload = "GameStateUnload"
 
 ---@param callback fun()
 ---@param once boolean
 ---@return StateAction
 function M.OnSaving(callback, once)
-    return StateAction.New(modeSave, callback, once)
+    return Event.On(modeSave, callback, once)
 end
 
 ---@param callback fun()
 ---@param once boolean
 ---@return StateAction
 function M.OnLoading(callback, once)
-    return StateAction.New(modeLoad, callback, once)
+    return Event.On(modeLoad, callback, once)
 end
 
 ---@param callback fun()
 ---@param once boolean
 ---@return StateAction
 function M.OnUnloading(callback, once)
-    return StateAction.New(modeUnload, callback, once)
-end
-
-local function runAction(mode, e)
-    for i, action in ipairs(actions) do
-        if action.Mode == mode then
-            action:Exec(e)
-        end
-    end
+    return Event.On(modeUnload, callback, once)
 end
 
 Ext.Events.GameStateChanged:Subscribe(function(e)
     if e.FromState == "Sync" and e.ToState == "Running" then
         Utils.Log.Info("Game Loaded.")
-        runAction(modeLoad, e)
+        Event.Trigger(modeLoad, e)
     elseif e.FromState == "Running" and e.ToState == "Save" then
         Utils.Log.Info("Saving started.")
-        runAction(modeSave, e)
+        Event.Trigger(modeSave, e)
     elseif e.FromState == "Save" and e.ToState == "Running" then
         Utils.Log.Info("Saving finished.")
-        runAction(modeLoad, e)
+        Event.Trigger(modeLoad, e)
     elseif e.FromState == "Running" and e.ToState == "UnloadLevel" then
         Utils.Log.Info("Level unloading.")
-        runAction(modeUnload, e)
+        Event.Trigger(modeUnload, e)
     elseif e.FromState == "UnloadSession" and e.ToState == "LoadSession" then
         Utils.Log.Info("Loading another save.")
-        runAction(modeUnload, e)
+        Event.Trigger(modeUnload, e)
     end
 end)
 
