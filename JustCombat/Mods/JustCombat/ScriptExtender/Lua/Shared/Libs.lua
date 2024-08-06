6ae9a776b32c284319fb2028d14d9a85ccda64ed
Tobias S <tobias.seifert97@gmail.com>
2024-08-06 22:07:02 +0200

    prepare user configuration

--- JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/Libs.lua
+++ JustCombat/Mods/JustCombat/ScriptExtender/Lua/Shared/Libs.lua
@@ -27,7 +27,7 @@ function M.Object(props)
 
     function Object.Init(values)
         if values ~= nil then
-            assert(type(values) == "table", "Object.Init - table expected, got " .. type(values))
+            assert(type(values) == "table", "Object.Init(values) - table expected, got " .. type(values))
         end
 
         local obj = {}
@@ -46,6 +46,161 @@ function M.Object(props)
     end
 
     return Object
+end
+
+---@param typeDefs table<number, table<string, string|function|table|LibsTypedTable>>|LibsTypedTable
+---@return LibsTypedTable
+function M.TypedTable(typeDefs, repeatable)
+    assert(type(typeDefs) == "table", "Libs.TypedTable(typeDefs, ...) - table expected, got " .. type(typeDefs))
+    assert(
+        type(repeatable) == "boolean" or repeatable == nil,
+        "Libs.TypedTable(..., repeatable) - boolean expected, got " .. type(typeDefs)
+    )
+
+    if typeDefs._IsTypedTable then
+        typeDefs = { typeDefs }
+    end
+
+    if repeatable and #typeDefs ~= 1 then
+        error("Libs.TypedTable - repeatable table must have exactly one type definition")
+    end
+
+    ---@class LibsTypedTable : LibsObject
+    ---@field Validate fun(table: table): boolean
+    ---@field TypeCheck fun(key: string, value: any): boolean
+    local Object = Libs.Object({
+        _IsTypedTable = true,
+        _TypeDefs = {},
+        _Repeatable = false,
+    })
+
+    ---@param key string
+    ---@param value any
+    ---@return boolean, string
+    function Object:TypeCheck(key, value)
+        assert(
+            type(key) == "string" or type(key) == "number",
+            "Libs.TypedTable:TypeCheck(key, ...) - string or number expected, got " .. type(key)
+        )
+
+        local typeDef = self._TypeDefs[key]
+        if typeDef == nil then
+            return false
+        end
+
+        assert(
+            type(typeDef) == "table",
+            "Libs.TypedTable.typeDefs[" .. key .. "] - table expected, got " .. type(typeDef)
+        )
+
+        local matchType = {
+            ["string"] = function()
+                return type(value) == "string"
+            end,
+            ["number"] = function()
+                return type(value) == "number"
+            end,
+            ["boolean"] = function()
+                return type(value) == "boolean"
+            end,
+            ["table"] = function()
+                return type(value) == "table"
+            end,
+            ["function"] = function()
+                return type(value) == "function"
+            end,
+            ["nil"] = function()
+                return value == nil
+            end,
+        }
+
+        if typeDef._IsTypedTable then
+            typeDef = { typeDef }
+        end
+
+        function test(validator)
+            if type(validator) == "string" then
+                if matchType[validator] and matchType[validator]() then
+                    return true, type(value)
+                end
+                return false, validator .. " expected, got " .. type(value)
+            end
+
+            if type(validator) == "function" then
+                local ok, res = pcall(validator, value)
+                return ok and res and true or false, res
+            end
+
+            -- basically enum or reference to another TypedTable
+            if type(validator) == "table" then
+                if validator._IsTypedTable then
+                    if type(value) ~= "table" then
+                        return false, "table expected, got " .. type(value)
+                    end
+                    return Object.Init(validator):Validate(value)
+                end
+                for _, enum in pairs(validator) do
+                    if value == enum then
+                        return true, value
+                    end
+                end
+                return false, "Value not in enum"
+            end
+
+            return false
+        end
+
+        local valid, result
+        for _, v in ipairs(typeDef) do
+            valid, result = test(v)
+            if valid then
+                return true, result
+            end
+        end
+
+        return false, result
+    end
+
+    function Object:Validate(tableToValidate)
+        assert(
+            type(tableToValidate) == "table",
+            "Libs.TypedTable:Validate(tableToValidate) - table expected, got " .. type(tableToValidate)
+        )
+
+        if self._TypeDefs._IsTypedTable then
+            self._TypeDefs = { self._TypeDefs }
+        end
+
+        local failed = {}
+        local function validate(toCheck)
+            for k, _ in pairs(self._TypeDefs) do
+                local valid, error = self:TypeCheck(k, toCheck[k])
+                if not valid then
+                    error = error or "Type mismatch"
+                    -- table.insert(failed, { [tostring(k)] = error })
+                    failed[tostring(k)] = error
+                end
+            end
+        end
+
+        if self._Repeatable then
+            if Utils.Table.Size(tableToValidate) == 0 then
+                validate({})
+            end
+            for _, v in pairs(tableToValidate) do
+                validate({ v })
+            end
+        else
+            validate(tableToValidate)
+        end
+
+        return Utils.Table.Size(failed) == 0, failed
+    end
+
+    return Object.Init({
+        _TypeDefs = typeDefs,
+        _Repeatable = repeatable and true or false,
+    })
 end
 
 return M
